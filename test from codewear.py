4aa20436145cff41fa0a18632a3522827fea4651
Anton <avkokin@gmail.com>
2020-01-23 23:34:23 +0300

    write function

--- test from codewear.py
+++ test from codewear.py
@@ -5,6 +5,23 @@
 # filter_list([1,'a','b',0,15]) == [1,0,15]
 # filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 #
-a = [1, 2, 'a', 'b']
-b = [1, 'a', 'b', 0, 15]
-c = [1, 2, 'aasf', '1', '123', 123]
+from typing import List, Union
+#
+# a = [1, 2, 'a', 'b']
+# b = [1, 'a', 'b', 0, 15]
+# c = [1, 2, 'aasf', '1', '123', 123]
+#
+#
+# def list_only_digit(lst):
+#     """function that takes a list of non-negative integers and strings and returns a new list with the strings
+#     filtered out
+#     Функция, которая возращает новый список, состоящий только из цифр
+#     """
+#     dig_list = []
+#     for elem in lst:
+#         if type(elem) == int:
+#             dig_list.append(elem)
+#     return print(dig_list)
+#
+#
+# list_only_digit(c)
