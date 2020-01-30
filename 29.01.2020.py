7e69a14da96f5378019e3c36fce2194a09289f02
Anton <avkokin@gmail.com>
2020-01-30 23:36:12 +0300

    update

--- 29.01.2020.py
+++ 29.01.2020.py
@@ -1,29 +1,29 @@
-data = {
-    "123": {"name": "Иванов", "email": "email@mail.ru"},
-    "321": {"name": "Петров", "email": "yandex@mail.ru"},
-    "432": {"name": "Василий", "email": "google@mail.ru"}
-}
+# data = {
+#     "123": {"name": "Иванов", "email": "email@mail.ru"},
+#     "321": {"name": "Петров", "email": "yandex@mail.ru"},
+#     "432": {"name": "Василий", "email": "google@mail.ru"}
+# }
 
 # for key, val in data.items():
 # print(key,':', val)
 # print(val.get('name'), ':', val.get('email'))
 
-my_str = "ok ret. pert"
+# my_str = "ok ret. pert"
 # print(my_str.rpartition('.'))
 
-s = "Today is a good day"
+# s = "Today is a good day"
 # print(s.partition("good"))
 
-str = "Я недаром вздрогнул, не загробный вздор, в порт, горящий как расплавленное лето, разворачивался и входил товарищ Теодор Нетте"
+# str = "Я недаром вздрогнул, не загробный вздор, в порт, горящий как расплавленное лето, разворачивался и входил товарищ Теодор Нетте"
 # print(str.partition(','))
-
-example = {"count": "5", "list": [
-    {"Name": "name1", "Surname": "surname1", "Age": "age1"},
-    {"Name": "name2", "Surname": "surname2", "Age": "age2"},
-    {"Name": "name3", "Surname": "surname3", "Age": "age3"},
-    {"Name": "name4", "Surname": "surname4", "Age": "age4"},
-    {"Name": "name5", "Surname": "surname5", "Age": "age5"}
-]}
+#
+# example = {"count": "5", "list": [
+#     {"Name": "name1", "Surname": "surname1", "Age": "age1"},
+#     {"Name": "name2", "Surname": "surname2", "Age": "age2"},
+#     {"Name": "name3", "Surname": "surname3", "Age": "age3"},
+#     {"Name": "name4", "Surname": "surname4", "Age": "age4"},
+#     {"Name": "name5", "Surname": "surname5", "Age": "age5"}
+# ]}
 
 # Найти словарь по значению ключа "surname5"
 # def func():
@@ -43,8 +43,8 @@ example = {"count": "5", "list": [
 # print(func2())
 
 
-result = next(d['Age'] for d in example['list'] if d.get('Surname') == 'surname3')
-print(result)
+# result = next(d['Age'] for d in example['list'] if d.get('Surname') == 'surname3')
+# print(result)
 
 # mytuple = ("яблоко", "банан", "вишня")
 # myIt = iter(mytuple)
