fc5a9e43b0e47ba1bcab60bc87efaaf954734e0c
avk512 <avkokin@gmail.com>
2020-03-14 18:02:50 +0300

    Решение ДЗ к лекции 3.1 "Работа с разными форматами данных" (обновлено)

--- 03.Marth/hw_3.1_json_xml.py
+++ 03.Marth/hw_3.1_json_xml.py
@@ -22,29 +22,13 @@ filename_xml = "newsafr.xml"
 # # без изменения первых букв (результат несколько другой)
 # wordslist = [word for word in allwordslist if len(word) >= 6]
 #
-# # 1) решение задачи с помощью модуля Counter
-# # wdc = Counter(wordslist)
-# # wl = Counter(wordslist).most_common(10)
-# # for s in wl:
-# #     print(f"Слово '{s[0]}' встретилось в тексте {s[1]} раз(а)")
+# 1) решение задачи с помощью модуля Counter
+# wdc = Counter(wordslist)
+# wl = Counter(wordslist).most_common(10)
+# for s in wl:
+#     print(f"Слово '{s[0]}' встретилось в тексте {s[1]} раз(а)")
 #
-# # 2) решение задачи без использования модуля Counter
-# words_count = {}
-# for word in wordslist:
-#     words_count[word] = wordslist.count(
-#         word)  # создаем словарь с парами: слово: количество
-#
-#
-# # функция выбора 2-го значения из кортежа (с количеством-частотой упоминания слова)
-# def mysort(tuple):
-#     return tuple[1]  # возврат из кортежа 2-го значения (частота упоминания слова)
-#
-#
-# # функция sorted возвращает из словаря список кортежей, которые сортируются по частоте упоминания слова
-# wrd = sorted(words_count.items(), key=mysort, reverse=True)
-# sl = wrd[0:10]  # срез кортежа от первого значения до 10 включительно
-# for i in sl:
-#     print(f"Слово '{i[0]}' встретилось в тексте {i[1]} раз(а)")
+# 2) решение задачи без использования модуля Counter смотрите ниже в примере для xml-файла
 
 ##############################
 # ЗАДАЧА №2 (для xml файла) #
@@ -60,16 +44,7 @@ for item in items:
 
 allwordslist = ' '.join(txt).split()
 
-# создаем список слов длиннее 6 символов и у каждого слова первую букву делаем прописной (для корректности подсчета)
-# wordslist = [word.capitalize() for word in allwordslist if len(word) >= 6]
-# без изменения первых букв (результат несколько другой)
 wordslist = [word for word in allwordslist if len(word) >= 6]
-
-# 1) решение задачи с помощью модуля Counter
-# wdc = Counter(wordslist)
-# wl = Counter(wordslist).most_common(10)
-# for s in wl:
-#     print(f"Слово '{s[0]}' встретилось в тексте {s[1]} раз(а)")
 
 # 2) решение задачи без использования модуля Counter
 words_count = {}
