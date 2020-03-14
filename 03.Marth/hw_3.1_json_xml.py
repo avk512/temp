5d4c10d54637e3dc5cf9e40a22079e39f8ef714b
avk512 <avkokin@gmail.com>
2020-03-14 14:44:17 +0300

    Решена задача №1 из 2-х

---
+++ 03.Marth/hw_3.1_json_xml.py
@@ -0,0 +1,51 @@
+import json
+from pprint import pprint
+from collections import Counter
+
+filename_json = "newsafr.json"
+filename_xml = "newsafr.xml"
+
+##############################
+# ЗАДАЧА №1 (для json файла) #
+##############################
+with open(filename_json, encoding="utf-8-sig") as f:
+    json_content = json.load(f)
+    data = json_content['rss']['channel']['items']
+    txt = [desc['description'] for desc in data]  # получаем список описаний
+
+# получаем список всех слов из описаний
+allwordslist = ' '.join(txt).split()
+
+# создаем список слов длиннее 6 символов и у каждого слова первую букву делаем прописной (для корректности подсчета)
+# wordslist = [word.capitalize() for word in allwordslist if len(word) >= 6]
+# без изменения первых букв (результат несколько другой)
+wordslist = [word for word in allwordslist if len(word) >= 6]
+
+# 1) решение задачи с помощью модуля Counter
+# wdc = Counter(wordslist)
+# wl = Counter(wordslist).most_common(10)
+# for s in wl:
+#     print(f"Слово '{s[0]}' встретилось в тексте {s[1]} раз(а)")
+
+# 2) решение задачи без использования модуля Counter
+words_count = {}
+for word in wordslist:
+    words_count[word] = wordslist.count(
+        word)  # создаем словарь с парами: слово: количество
+
+
+# функция выбора 2-го значения из кортежа (с количеством-частотой упоминания слова)
+def mysort(tuple):
+    return tuple[
+        1]  # возврат из кортежа 2-го значения (частота упоминания слова)
+
+
+# функция sorted возвращает из словаря список кортежей, которые сортируются по частоте упоминания слова
+wrd = sorted(words_count.items(), key=mysort, reverse=True)
+sl = wrd[0:10]  # срез кортежа от первого значения до 10 включительно
+for i in sl:
+    print(f"Слово '{i[0]}' встретилось в тексте {i[1]} раз(а)")
+
+##############################
+# ЗАДАЧА №2 (для xml файла) #
+##############################
