e2ef3a81220ad9e4cd1a219a1768c275aea20a64
avk512 <avkokin@gmail.com>
2020-03-16 23:04:23 +0300

    Решение ДЗ к лекции 3.1 "Работа с разными форматами данных" (доработанная версия)

--- 03.Marth/hw_3.1_json_xml.py
+++ 03.Marth/hw_3.1_json_xml.py
@@ -6,61 +6,70 @@ from collections import Counter
 filename_json = "newsafr.json"
 filename_xml = "newsafr.xml"
 
-##############################
-# ЗАДАЧА №1 (для json файла) #
-##############################
-# with open(filename_json, encoding="utf-8-sig") as f:
-#     json_content = json.load(f)
-#     data = json_content['rss']['channel']['items']
-#     txt = [desc['description'] for desc in data]  # получаем список описаний
-#
-# # получаем список всех слов из описаний
-# allwordslist = ' '.join(txt).split()
-#
-# # создаем список слов длиннее 6 символов и у каждого слова первую букву делаем прописной (для корректности подсчета)
-# # wordslist = [word.capitalize() for word in allwordslist if len(word) >= 6]
-# # без изменения первых букв (результат несколько другой)
-# wordslist = [word for word in allwordslist if len(word) >= 6]
-#
-# 1) решение задачи с помощью модуля Counter
-# wdc = Counter(wordslist)
-# wl = Counter(wordslist).most_common(10)
-# for s in wl:
-#     print(f"Слово '{s[0]}' встретилось в тексте {s[1]} раз(а)")
-#
-# 2) решение задачи без использования модуля Counter смотрите ниже в примере для xml-файла
 
-##############################
-# ЗАДАЧА №2 (для xml файла) #
-##############################
-
-tree = ET.parse(filename_xml)
-root = tree.getroot()
-items = root.findall("channel/item")
-
-txt = []
-for item in items:
-    txt.append(item.find('description').text)
-
-allwordslist = ' '.join(txt).split()
-
-wordslist = [word for word in allwordslist if len(word) >= 6]
-
-# 2) решение задачи без использования модуля Counter
-words_count = {}
-for word in wordslist:
-    words_count[word] = wordslist.count(
-        word)  # создаем словарь с парами: слово: количество
+def json_parsing(file):
+    with open(file, encoding="utf-8-sig") as f:
+        json_content = json.load(f)
+        data = json_content['rss']['channel']['items']
+        txt = [desc['description'] for desc in data]
+    allwordslist = ' '.join(txt).split()
+    wordslist = [word.capitalize() for word in allwordslist if len(word) >= 6]
+    return wordslist
 
 
-# функция выбора 2-го значения из кортежа (с количеством-частотой упоминания слова)
+def xml_parsing(file):
+    tree = ET.parse(file)
+    root = tree.getroot()
+    items = root.findall('channel/item')
+    txt = []
+    for item in items:
+        txt.append(item.find('description').text)
+    allwordslist = ' '.join(txt).split()
+    wordslist = [word.capitalize() for word in allwordslist if len(word) >= 6]
+    return wordslist
+
+
+def words_with_Counter(func_parsing):
+    """Функция выборки слов с использованием модуля Counter"""
+    wl = Counter(func_parsing).most_common(10)
+    for s in wl:
+        print(f"Слово '{s[0]}' встретилось в тексте {s[1]} раз(а)")
+
+
+def words_dict(func_parsing):
+    """Функция создания словаря с парами 'слово: количество'"""
+    words_count = {}
+    for word in func_parsing:
+        words_count[word] = func_parsing.count(word)
+    return words_count
+
+
 def mysort(tuple):
-    return tuple[
-        1]  # возврат из кортежа 2-го значения (частота упоминания слова)
+    """Функция сортировки кортежа"""
+    return tuple[1]
 
 
-# функция sorted возвращает из словаря список кортежей, которые сортируются по частоте упоминания слова
-wrd = sorted(words_count.items(), key=mysort, reverse=True)
-sl = wrd[0:10]  # срез кортежа от первого значения до 10 включительно
-for i in sl:
-    print(f"Слово '{i[0]}' встретилось в тексте {i[1]} раз(а)")
+def words_sort(func_dict, func_tuple):
+    """Функция выборки слов без использования модуля Counter"""
+    wrd = sorted(func_dict.items(), key=func_tuple, reverse=True)
+    sl = wrd[0:10]
+    for i in sl:
+        print(f"Слово '{i[0]}' встретилось в тексте {i[1]} раз(а)")
+
+
+a = json_parsing(filename_json)
+b = xml_parsing(filename_xml)
+c = words_dict(a)
+d = words_dict(b)
+
+print("Выборка слов при парсинге JSON-файла (используем модуль Counter:)")
+words_with_Counter(a)
+print()
+print("Выборка слов при парсинге XML-файла (используем модуль Counter:)")
+words_with_Counter(b)
+print()
+print("Выборка слов при парсинге JSON-файла (вручную):")
+words_sort(c, mysort)
+print()
+print("Выборка слов при парсинге XML-файла (вручную):")
+words_sort(d, mysort)
